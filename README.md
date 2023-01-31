b3f32f1bfbd886813832608d3bdf86985edfc777
Jakub Musialowski <mds@mds.biz.pl>
2023-01-31 22:13:48 +0100

    Readme file added

---
+++ README.md
@@ -0,0 +1,25 @@
+## Tasks List App
+---
+Welcome to my tasks list app making during Frontend Developer course on YouCode.pl.
+
+## Demo
+---
+[https://jakubm-i.github.io/yc-todo-app/](https://jakubm-i.github.io/yc-todo-app/)
+
+![My page](graphics/task-list.gif)
+
+## About my page
+---
+Tasks list allows adding tasks to list with assignment one of 3 priority levels - height, average, no priority. Task on the list are sorted by priority - from height priority to no priority, and by done status. Done tasks goes on the bottom of the list. 
+
+Tasks can be marked as done and removed from the list. 
+
+## Techniques
+---
+- CSS
+- Flex
+- CSS function min, max
+- CSS variables
+- CSS transitions
+- ES6+ feature
+- BEM
--- script.js
+++ script.js
@@ -1,116 +1,117 @@
-const tasksList = [];
+{
+    const tasksList = [];
 
-const render = () =>{
-    let listHTML = "";
+    const render = () => {
+        let listHTML = "";
 
-    for (const task of tasksList) {
-        listHTML +=`<li class="tasks__listItem ${task.taskDone ? "tasks__listItem--done" : ""}">
+        for (const task of tasksList) {
+            listHTML += `<li class="tasks__listItem ${task.taskDone ? "tasks__listItem--done" : ""}">
         <button class="js-tasks__doneButton tasks__doneButton ${task.taskDone ? "tasks__doneButton--done" : ""}"></button>
         <span class="tasks__taskContent">${task.taskName}</span>
-        <div class="tasks__itemButtons"><span class="tasks__taskPriority ${task.taskPriority === 0 ? "tasks__taskPriority--nopriority" : task.taskPriority === 1 ? "tasks__taskPriority--averange" : "tasks__taskPriority--important" }"></span>
-        <button class="js-tasks__removeButton tasks__removeButton"></button></div></li>`;   
+        <div class="tasks__itemButtons"><span class="tasks__taskPriority ${task.taskPriority === 0 ? "tasks__taskPriority--nopriority" : task.taskPriority === 1 ? "tasks__taskPriority--averange" : "tasks__taskPriority--important"}"></span>
+        <button class="js-tasks__removeButton tasks__removeButton"></button></div></li>`;
+        };
+
+        document.querySelector(".js-tasksList").innerHTML = listHTML;
+
+        taskListUpdate();
     };
 
-    document.querySelector(".js-tasksList").innerHTML = listHTML;
+    const taskListUpdate = () => {
+        const doneButton = document.querySelectorAll(".js-tasks__doneButton");
 
-    taskListUpdate();   
-};
-
-const taskListUpdate = () => {
-    const doneButton = document.querySelectorAll(".js-tasks__doneButton");
-
-    doneButton.forEach((button, index) => {
-        button.addEventListener("click", () =>{
-            button.classList.toggle("tasks__doneButton--done");
-            doneTask(index);
-            listSort();
+        doneButton.forEach((button, index) => {
+            button.addEventListener("click", () => {
+                button.classList.toggle("tasks__doneButton--done");
+                doneTask(index);
+                listSort();
+            });
         });
-    });
 
-    const removeButton = document.querySelectorAll(".js-tasks__removeButton");
+        const removeButton = document.querySelectorAll(".js-tasks__removeButton");
 
-    removeButton.forEach((button, index) => {
-        button.addEventListener("click", () => {
-            removeTask(index);
+        removeButton.forEach((button, index) => {
+            button.addEventListener("click", () => {
+                removeTask(index);
+            });
         });
-    });
-};
+    };
 
-const itemSorting = (a, b) => {
-    return a.taskDone - b.taskDone || b.taskPriority - a.taskPriority;
-};
+    const itemSorting = (a, b) => {
+        return a.taskDone - b.taskDone || b.taskPriority - a.taskPriority;
+    };
 
-const listSort = () => {
-    tasksList.sort(itemSorting)
-    render();
-}
-
-const taskAdd = (newTask, prioritySelect) => {
-
-    const priorityLevel = prioritySelect.value;
-
-    const newTaskPriority = () => {
-        switch (priorityLevel) {
-            case "":
-                return 0;
-            case "brak":
-                return 0;
-            case "sredni":
-                return 1;
-            case "wysoki":
-                return 2;
-        }
+    const listSort = () => {
+        tasksList.sort(itemSorting)
+        render();
     }
 
-    tasksList.push(
-        {
-            taskName: newTask,
-            taskPriority: newTaskPriority(),
-            taskDone: false,
+    const taskAdd = (newTask, prioritySelect) => {
+
+        const priorityLevel = prioritySelect.value;
+
+        const newTaskPriority = () => {
+            switch (priorityLevel) {
+                case "":
+                    return 0;
+                case "brak":
+                    return 0;
+                case "sredni":
+                    return 1;
+                case "wysoki":
+                    return 2;
+            }
         }
-    );
-    render();
-};
 
-const formFieldErase = (newTaskField, prioritySelect) => {
-    newTaskField.value = "";
-    prioritySelect.value = "";
-};
-
-const doneTask = (index) => {
-    tasksList[index].taskDone = !tasksList[index].taskDone;
-    render();
-};
-
-const removeTask = (index) => {
-    tasksList.splice(index, 1);
-    render();
-};
-
-const formSubmit = (e) => {
-    e.preventDefault();
-
-    const newTaskField = document.querySelector(".js-addNewTask");
-    const prioritySelect = document.querySelector(".js-newTaskPriority");
-    const newTask = newTaskField.value.trim();
-
-    if(newTask === ""){
-        return
+        tasksList.push(
+            {
+                taskName: newTask,
+                taskPriority: newTaskPriority(),
+                taskDone: false,
+            }
+        );
+        render();
     };
 
-    taskAdd(newTask, prioritySelect);
-    listSort();
-    formFieldErase(newTaskField, prioritySelect);
-    newTaskField.focus();
+    const formFieldErase = (newTaskField, prioritySelect) => {
+        newTaskField.value = "";
+        prioritySelect.value = "";
+    };
+
+    const doneTask = (index) => {
+        tasksList[index].taskDone = !tasksList[index].taskDone;
+        render();
+    };
+
+    const removeTask = (index) => {
+        tasksList.splice(index, 1);
+        render();
+    };
+
+    const formSubmit = (e) => {
+        e.preventDefault();
+
+        const newTaskField = document.querySelector(".js-addNewTask");
+        const prioritySelect = document.querySelector(".js-newTaskPriority");
+        const newTask = newTaskField.value.trim();
+
+        if (newTask === "") {
+            return
+        };
+
+        taskAdd(newTask, prioritySelect);
+        listSort();
+        formFieldErase(newTaskField, prioritySelect);
+        newTaskField.focus();
+    }
+
+    const init = () => {
+        render();
+
+        const addForm = document.querySelector(".js-taskListForm");
+        addForm.addEventListener("submit", formSubmit);
+
+    };
+
+    init();
 }
-
-const init = () => {
-    render();
-
-    const addForm = document.querySelector(".js-taskListForm");
-    addForm.addEventListener("submit", formSubmit);
-
-    // doneTaskSorting();
-};
-
-init();
